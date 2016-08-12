0b16bc48fe520b2e333a2d108151bbe04ada7e52
zhangzju <zhangway19921221@163.com>
2016-08-13 02:53:25 +0800

    v0.01, add format directory

---
+++ res/cao.py
@@ -0,0 +1,14 @@
+from flask_restful import Resource
+
+class Cao(Resource):
+    def get(self):
+        pass
+    def post(self):
+        pass
+
+
+class Hehe(Resource):
+    def get(self):
+        pass
+    def post(self):
+        pass
