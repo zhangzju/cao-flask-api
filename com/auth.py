5aeb17be3205353e3e0d3683bf37a5b7c084f06d
zhangzju <zhangway19921221@163.com>
2016-09-29 23:12:10 +0800

    refactoring this project

--- com/auth.py
+++
@@ -1,7 +0,0 @@
-from flask_restful import Resource
-
-class Login(Resource):
-    def get(self):
-        pass
-    def post(self):
-        pass
--- res/contact.py
+++
@@ -1,7 +0,0 @@
-from flask_restful import Resource
-
-class Fellow(Resource):
-    def get(self):
-        pass
-    def post(self):
-        pass
